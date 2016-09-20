efabafafc431c4a9ef73d31d310a293dafae72f3
= <danielmonteiro@id.uff.br>
2016-09-20 17:35:36 -0300

    fix windows compilation of exported functions for proper JS<->C++ communication

--- build_windows.bat
+++ build_windows.bat
@@ -1,3 +1,3 @@
 cls
 del game.htmp game.js
-em++ -std=c++0x -s USE_ZLIB=1 -s USE_LIBPNG=1 -s FULL_ES2=1 --preload-file res --use-preload-plugins -Iincludes -IGame -Iincludes/glm-trunk boilerplate/main.cpp boilerplate/render.cpp boilerplate/x11.cpp boilerplate/GLES2Lesson.cpp  Game/Game.cpp -o game.html
+em++ -std=c++0x -s EXPORTED_FUNCTIONS="['_main', '_int_sqrt']" -s USE_ZLIB=1 -s USE_LIBPNG=1 -s FULL_ES2=1 --preload-file res --use-preload-plugins -Iincludes -IGame -Iincludes/glm-trunk boilerplate/main.cpp boilerplate/render.cpp boilerplate/x11.cpp boilerplate/GLES2Lesson.cpp  Game/Game.cpp -o game.html
